6803f05c90571e74234984a64027281ea07e8820
abacusus <namankumar0063@gmail.com>
2026-01-13 19:43:54 +0530

    added requirements.txt

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\deepak\OneDrive\Desktop\riasecPrototype\venv
+set VIRTUAL_ENV=C:\Users\Home\Desktop\TECHSTUFF\deepakcreations\riasecModel\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
